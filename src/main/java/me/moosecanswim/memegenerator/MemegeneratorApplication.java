e1df02052f6c97cab4a3eebc784be135ae2c9435
moosecanswim <kyle.gallagher.09@gmail.com>
2017-09-12 14:18:47 -0400

    Initial commit

---
+++ src/main/java/me/moosecanswim/memegenerator/MemegeneratorApplication.java
@@ -0,0 +1,12 @@
+package me.moosecanswim.memegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemegeneratorApplication.class, args);
+	}
+}
